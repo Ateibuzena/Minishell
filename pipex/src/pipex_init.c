36a381c1e8c5b54eb966858da8bfc60c037b115d
Ana Zubieta Pena <azubieta@c1r6s6.42malaga.com>
2025-03-16 19:19:00 +0100

    norminette a pipex_init

--- pipex/src/pipex_init.c
+++ pipex/src/pipex_init.c
@@ -1,54 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azubieta <azubieta@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 19:15:39 by azubieta          #+#    #+#             */
+/*   Updated: 2025/03/16 19:18:28 by azubieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../pipexft.h"
 
 // Contar comandos
-static void ft_count_cmd(t_pipex *pipex, char *input[])
+static void	ft_count_cmd(t_pipex *pipex, char *input[])
 {
-	int i;
+	int	i;
 
 	i = 0;
 	pipex->n = 0;
-    while (input[i])
+	while (input[i])
 	{
-        if (ft_strcmp(input[i], "|") != 0)
-            pipex->n++;
+		if (ft_strcmp(input[i], "|") != 0)
+			pipex->n++;
 		i++;
-    }
-    if (pipex->n)
+	}
+	if (pipex->n)
 		pipex->n += 1;
 }
 
-// Inicializar estructura pipex
-void ft_init(t_pipex *pipex, char *input[], t_History *history)
+// Asigna memoria para pipex->argv
+static int	ft_alloc_argv(t_pipex *pipex, char *input[])
 {
-	int i;
+	int	i;
 
 	i = 0;
-    pipex->status = 0;
+	pipex->argv = malloc(sizeof(char *) * (ft_strlen_double(input) + 1));
+	if (!pipex->argv)
+		return (ft_perror("Malloc error: argv\n"), 0);
+	while (input[i])
+	{
+		pipex->argv[i] = ft_strdup(input[i]);
+		if (!pipex->argv[i])
+			return (ft_perror("Malloc error: argv[i]\n"), 0);
+		i++;
+	}
+	pipex->argv[i] = NULL;
+	pipex->size = i;
+	return (1);
+}
+
+// Asigna memoria para pipex->pids
+static int	ft_alloc_pids(t_pipex *pipex)
+{
+	pipex->pids = malloc(pipex->n * sizeof(pid_t));
+	if (!pipex->pids)
+		return (ft_perror("Malloc error: pids\n"), 0);
+	return (1);
+}
+
+// Inicializar estructura pipex
+void	ft_init(t_pipex *pipex, char *input[], t_History *history)
+{
+	int	i;
+
+	pipex->status = 0;
 	pipex->cmd = -1;
-    pipex->history = history;
-    pipex->i = 0;
-    pipex->count = 0;
+	pipex->history = history;
+	pipex->i = 0;
+	pipex->count = 0;
 	pipex->infile = STDIN_FILENO;
 	pipex->outfile = STDOUT_FILENO;
 	ft_count_cmd(pipex, input);
-    
-    // Reservar memoria para argv
-    pipex->argv = (char **)malloc(sizeof(char *) * (ft_strlen_double(input) + 1));
-    if (!pipex->argv)
-    return (ft_perror("Malloc error: argv\n"), ft_free_pipex(&pipex), exit(EXIT_FAILURE));
-    
-    while (input[i])
-	{
-        pipex->argv[i] = ft_strdup(input[i]); // strdup evita malloc + strcpy manual
-        if (!pipex->argv[i])
-            return (ft_perror("Malloc error: argv[i]\n"), ft_free_pipex(&pipex), exit(EXIT_FAILURE));
-		i++;
-	}
-    pipex->argv[i] = NULL;
-    pipex->size = i;
-
-    // Reservar memoria para los PIDs
-    pipex->pids = (pid_t *)malloc(pipex->n * sizeof(pid_t));
-    if (!pipex->pids)
-        return (ft_perror("Malloc error: pids\n"), ft_free_pipex(&pipex), exit(EXIT_FAILURE));
+	if (!ft_alloc_argv(pipex, input) || !ft_alloc_pids(pipex))
+		return (ft_free_pipex(pipex), exit(EXIT_FAILURE));
 }
