fdfa111dabb51ee460648ee8a90d2e8ee23ea7eb
Ana Zubieta Pena <azubieta@c3r1s2.42malaga.com>
2025-04-12 21:39:34 +0200

    modificando funcion ft_free_history en history para envitar acceder a una variable nula

--- history/src/history_free.c
+++ history/src/history_free.c
@@ -6,7 +6,7 @@
 /*   By: azubieta <azubieta@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 13:58:26 by azubieta          #+#    #+#             */
-/*   Updated: 2025/01/11 15:19:43 by azubieta         ###   ########.fr       */
+/*   Updated: 2025/04/12 20:08:46 by azubieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,17 @@ void	ft_free_history(t_History *history)
 	t_HistoryEntry	*current;
 	t_HistoryEntry	*next;
 
+	if (!history)
+		return ;
 	current = history->head;
 	while (current)
 	{
 		next = current->next;
-		free(current->line);
+		if (current->line)
+			free(current->line);
 		free(current);
 		current = next;
 	}
 	free(history);
 }
+
