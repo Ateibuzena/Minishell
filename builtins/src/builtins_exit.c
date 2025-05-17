c21f4ba7c795390b3437fa8c09913357260002dd
Ana Zubieta Pena <azubieta@c2r2s5.42malaga.com>
2025-05-17 19:28:45 +0200

    modificando ft_exit

--- builtins/src/builtins_exit.c
+++ builtins/src/builtins_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_exit.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azubieta <azubieta@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: azubieta <azubieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 16:05:19 by azubieta          #+#    #+#             */
-/*   Updated: 2025/05/02 23:06:16 by azubieta         ###   ########.fr       */
+/*   Updated: 2025/05/17 19:28:20 by azubieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,15 @@
 
 int	ft_exit(char **args)
 {
-	int		status;
-	char	*arg;
-	int		i;
-
-	i = 0;
-	status = 0;
 	if (args[1] != NULL)
 	{
-		arg = args[1];
-		while (arg[i] != '\0')
-		{
-			if (!ft_isdigit(arg[i]) && (arg[i] != '-' || i == 0))
-			{
-				ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-				ft_putstr_fd(arg, STDERR_FILENO);
-				ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-				return (1);
-			}
-			i++;
-		}
-		status = ft_atoi(args[1]);
+		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(args[1], STDERR_FILENO);
+		ft_putstr_fd(": not argument required\n", STDERR_FILENO);
+		g_exit = 1;
+		return (1);
 	}
-	//ft_putstr_fd("exit\n", STDOUT_FILENO);
-	if (status != 0)
-		g_exit = status;
-	else if (status == 0)
-		g_exit = -1;
-	return (status);
+	ft_putstr_fd("exit\n", STDOUT_FILENO);
+	g_exit = 1;
+	return (0);
 }
