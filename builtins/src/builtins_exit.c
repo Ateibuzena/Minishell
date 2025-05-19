3ee67bb0e748ca8a9a9b744d396b1ddab192867d
Ana Zubieta Pena <azubieta@c2r5s1.42malaga.com>
2025-05-19 15:40:13 +0200

    quitando printf en exit

--- builtins/src/builtins_exit.c
+++ builtins/src/builtins_exit.c
@@ -6,7 +6,7 @@
 /*   By: azubieta <azubieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 16:05:19 by azubieta          #+#    #+#             */
-/*   Updated: 2025/05/17 19:28:20 by azubieta         ###   ########.fr       */
+/*   Updated: 2025/05/19 15:39:27 by azubieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int	ft_exit(char **args)
 		g_exit = 1;
 		return (1);
 	}
-	ft_putstr_fd("exit\n", STDOUT_FILENO);
 	g_exit = 1;
 	return (0);
 }
